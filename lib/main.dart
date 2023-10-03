3841bb9b9d31051c86207d370a31c336a50c542f
Elena <elenaromadam@gmail.com>
2023-10-03 21:17:07 +0200

    commit firebase: he enlazado el proyecto con firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 
 import 'package:app/Actividad1.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(Actividad1());
+
 }
+
+
+
+
