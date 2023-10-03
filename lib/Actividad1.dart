bd839782158281e93fbeda41507ac7cd8bd344d2
Elena <elenaromadam@gmail.com>
2023-10-03 20:54:02 +0200

    commit actividad1

---
+++ lib/Actividad1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Actividad1 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    MaterialApp materialApp = MaterialApp(title: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+
+    );
+    return materialApp;
+
+  }
+}
