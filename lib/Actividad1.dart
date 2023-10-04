66a118d41c8480b3c5ba81829ea6c82eff6ce82e
Elena <elenaromadam@gmail.com>
2023-10-04 19:12:55 +0200

    commit MaterialApp: he puesto las rutas y la ruta inicial

--- lib/Actividad1.dart
+++ lib/Actividad1.dart
@@ -1,3 +1,6 @@
+import 'package:app/OnBoarding/HomeView.dart';
+import 'package:app/OnBoarding/LoginView.dart';
+import 'package:app/OnBoarding/RegisterView.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,12 +9,17 @@ class Actividad1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    MaterialApp materialApp = MaterialApp(title: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-
-
-
+    return MaterialApp(
+      title: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        routes: {
+          '/logingview': (context)=> LoginView(),
+          '/registergview': (context)=> RegisterView(),
+          '/homeview': (context)=> HomeView()
+        },
+      initialRoute: '/logingview',
+      debugShowCheckedModeBanner: false,
     );
-    return materialApp;
+
 
   }
 }
