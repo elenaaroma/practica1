49e3304636caf90513bf8df39323a4700bdfbf45
Elena <elenaromadam@gmail.com>
2023-10-08 16:43:09 +0200

    commit imagen en splashview

--- lib/Actividad1.dart
+++ lib/Actividad1.dart
@@ -1,6 +1,7 @@
 import 'package:app/Main/HomeView.dart';
 import 'package:app/OnBoarding/LoginView.dart';
 import 'package:app/OnBoarding/RegisterView.dart';
+import 'package:app/Splash/SplashView.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,9 +15,10 @@ class Actividad1 extends StatelessWidget {
         routes: {
           '/loginview': (context)=> LoginView(),
           '/registergview': (context)=> RegisterView(),
-          '/homeview': (context)=> HomeView()
+          '/homeview': (context)=> HomeView(),
+          '/splashview' : (context) => SplashView()
         },
-      initialRoute: '/loginview',
+      initialRoute: '/splashview',
       debugShowCheckedModeBanner: false,
     );
 
--- lib/Splash/SplashView.dart
+++ lib/Splash/SplashView.dart
@@ -2,10 +2,13 @@
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+
+    return Image.network("https://cdn.discordapp.com/attachments/812312382876942369/1160583034328854688/image.png?ex=6535302d&is=6522bb2d&hm=55523d3a86b5c5bb248ed54701932eb40a8b40ed06d85e9c37ee6d5815e1b90d&");
+
   }
 
 }
