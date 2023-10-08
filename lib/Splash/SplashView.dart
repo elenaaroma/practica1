a359160cd5c93b99db9be69a91d0757a61d2b50b
Elena <elenaromadam@gmail.com>
2023-10-08 17:34:25 +0200

    commit imagen en splashview desde carpeta resources

--- lib/Splash/SplashView.dart
+++ lib/Splash/SplashView.dart
@@ -7,7 +7,7 @@ class SplashView extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return Image.network("https://cdn.discordapp.com/attachments/812312382876942369/1160583034328854688/image.png?ex=6535302d&is=6522bb2d&hm=55523d3a86b5c5bb248ed54701932eb40a8b40ed06d85e9c37ee6d5815e1b90d&");
+    return Image.asset("resources/gatete.jpg");
 
   }
 
