863b0871189ceaaf9ebe02a4b17f4cf56ea03ee6
elena <elenaromadam@gmail.com>
2023-10-09 09:10:07 +0200

    commit rueda de progreso

--- lib/Splash/SplashView.dart
+++ lib/Splash/SplashView.dart
@@ -3,11 +3,29 @@ import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget{
 
+late BuildContext  _context;
+
+
+  void checkSesion () async{
+
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return Image.asset("resources/gatete.jpg");
+    _context = context;
+
+    Column column = Column(
+      children: [
+        Image.asset("resources/gatete.jpg", width: 400,height: 600),
+        CircularProgressIndicator()
+      ],
+    );
+
+    return column;
 
   }
 
