afee1b7a019b4870332cc67e65a7f1133e8218a4
Elena <elenaromadam@gmail.com>
2023-10-18 20:31:36 +0200

    commit appbar home

--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -1,18 +1,29 @@
 
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget{
+class HomeView extends StatefulWidget{
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView>{
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-
+    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+      appBar: AppBar(title: Text("HOME"),
         centerTitle: true,
         shadowColor: Colors.red[300],
-        backgroundColor: Colors.deepOrange[100],
-      ),
+        backgroundColor: Colors.deepOrange[100],)
+
     );
   }
 
