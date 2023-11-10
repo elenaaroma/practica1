79b9ee6b4973d0c6b1dc56d2c151a3a4236530dc
elenaaroma <elenaromadam@gmail.com>
2023-11-10 17:03:08 +0100

    commit botones de bottomMenu

--- lib/Custom/BottomMenu.dart
+++ lib/Custom/BottomMenu.dart
@@ -5,11 +5,25 @@ class BottomMenu extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Row( children: [
-      TextButton(onPressed: null, child: Text("Lista"),),
-      TextButton(onPressed: null, child: Text("Celdas"),),
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+      TextButton(onPressed: () => boton1Pressed, child: Icon(Icons.list, color: Colors.red[300],),),
+      TextButton(onPressed: () => boton2Pressed, child: Icon(Icons.grid_view, color: Colors.red[300],),),
+
     ]
     );
+  }
+
+  void boton1Pressed(){
+    botonesClick(0);
+  }
+  void boton2Pressed(){
+    botonesClick(1);
+  }
+
+  void botonesClick(int indice){
+    print(indice.toString());
   }
   
 }
