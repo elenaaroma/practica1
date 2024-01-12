285058475c3c1186858239339b639be2a6124b5a
Elena <elenaromadam@gmail.com>
2024-01-12 12:17:57 +0100

    clase FirebaseAdmin buscar posts

--- lib/Singletone/FirebaseAdmin.dart
+++ lib/Singletone/FirebaseAdmin.dart
@@ -1,7 +1,38 @@
-class FirebaseAdmin{
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  void holamundo(){
+import '../FirestoreObjects/FbPost.dart';
+import 'DataHolder.dart';
 
+class FirebaseAdmin {
+
+  Future<QuerySnapshot<FbPost>> getPosts() async {
+
+    CollectionReference<FbPost> reference = DataHolder().db.collection("Posts").withConverter(
+        fromFirestore: FbPost.fromFirestore,
+        toFirestore: (FbPost post, _) => post.toFirestore());
+
+    return await reference.get();
   }
 
+  void insertPost(FbPost post) {
+    CollectionReference<FbPost> postRef = DataHolder().db.collection('Posts').withConverter(
+        fromFirestore: FbPost.fromFirestore,
+        toFirestore: (FbPost post, _) => post.toFirestore()
+    );
+
+    postRef.add(post);
+  }
+
+  Future<List<Map<String, dynamic>>> getPostByTitle(String searchValue) async {
+    QuerySnapshot querySnapshot = await DataHolder().db
+        .collection('Posts')
+        .where('titulo', isGreaterThanOrEqualTo: searchValue)
+        .get();
+
+    return querySnapshot.docs
+        .where((doc) =>
+        (doc['titulo'] as String).contains(searchValue))
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
+  }
 }
